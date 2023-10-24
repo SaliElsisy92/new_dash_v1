7d1bffc6eae8124b7b0d9a41110429710bc8e51a
Sali.Elsisy <sali.elsisy@pioneers-solutions.com>
2023-10-24 10:53:36 +0300

    translation-package

--- app/Dash/DashResources.php
+++ app/Dash/DashResources.php
@@ -5,7 +5,6 @@ return [
      \App\Dash\Resources\AdminGroups::class,
      \App\Dash\Resources\Admins::class,
      \App\Dash\Resources\Users::class,
-    \App\Dash\Resources\soulution_cat::class,
     \App\Dash\Resources\improvement::class,
     \App\Dash\Resources\setting::class,
     // Users End//
--- database/migrations/2023_10_16_074158_create_soulution_cats_table.php
+++ database/migrations/2023_10_16_074158_create_soulution_cats_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('soulution_cats', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
             $table->string('photo')->nullable();
             $table->softDeletes();
             $table->timestamps();

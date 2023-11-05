46245fa939c2cfcb5533ace4832f19c05c9b6293
asmaazaki <asmaazaki3232@gmail.com>
2023-11-05 21:32:06 +0200

    front

---
+++ Modules/FrontendModule/Http/Controllers/FrontendModuleController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Modules\FrontendModule\Http\Controllers;
+
+use Illuminate\Contracts\Support\Renderable;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use App\Models\Solution;
+use App\Models\Service;
+use App\Models\Management;
+use App\Models\ManPowerService;
+
+class FrontendModuleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     * @return Renderable
+     */
+    public function index()
+    {
+        // here we will return the all data in the tables
+        return view('frontendmodule::frontend.front');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     * @return Renderable
+     */
+    public function create()
+    {
+        return view('frontendmodule::create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     * @param Request $request
+     * @return Renderable
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Show the specified resource.
+     * @param int $id
+     * @return Renderable
+     */
+    public function show($id)
+    {
+        return view('frontendmodule::show');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     * @param int $id
+     * @return Renderable
+     */
+    public function edit($id)
+    {
+        return view('frontendmodule::edit');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     * @param Request $request
+     * @param int $id
+     * @return Renderable
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     * @param int $id
+     * @return Renderable
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ Modules/FrontendModule/Resources/views/frontend/front.blade.php
@@ -0,0 +1,294 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Modern Pioneers</title>
+    <!-- google font -->
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"
+        rel="stylesheet" />
+    <!-- FontAwesome -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" rel="stylesheet" />
+    <!-- main css -->
+    <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet" />
+</head>
+
+<body>
+    <section class="home-wrapper">
+        <div class="page-container">
+            <!-- Hexagon -->
+
+            <div class="hexagon-container opened">
+                <div class="hexagon main-hexagon animated">
+                    <span class="hex-title" data-tippy-content="About Us"><i
+                            class="fa-regular fa-address-card"></i></span>
+
+                    <!-- Sub Categories -->
+                    <div class="sub-categories">
+                        <div class="hexagon">
+                            <span class="hex-title" data-tippy-content="Career"><i class="fa-solid fa-eject"></i></span>
+
+                            <!-- Sub Sub Categories -->
+                            <div class="sub-sub-category">
+                                <div class="hexagon" data-id="careers">
+                                    <span class="hex-title" data-tippy-content="Career"><i
+                                            class="fa-solid fa-eject"></i></span>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="hexagon">
+                            <span class="hex-title" data-tippy-content="Contact Us"><i
+                                    class="fa-solid fa-headset"></i></span>
+                            <!-- Sub Sub Categories -->
+                            <div class="sub-sub-category">
+                                <div class="hexagon" data-id="contact">
+                                    <span class="hex-title" data-tippy-content="Contact Us"><i
+                                            class="fa-solid fa-headset"></i></span>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="hexagon">
+                            <span class="hex-title" data-tippy-content="About Us"><i
+                                    class="fa-regular fa-address-card"></i></span>
+                            <!-- Sub Sub Categories -->
+                            <div class="sub-sub-category">
+                                <div class="hexagon" data-id="vision">
+                                    <span class="hex-title" data-tippy-content="Our Vision"><i
+                                            class="fa-solid fa-arrows-to-eye"></i></span>
+                                </div>
+                                <div class="hexagon" data-id="mission">
+                                    <span class="hex-title" data-tippy-content="Our Mission"><i
+                                            class="fa-solid fa-bullseye"></i></span>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="hexagon">
+                            <span class="hex-title" data-tippy-content="News & Events"><i
+                                    class="fa-regular fa-newspaper"></i></span>
+                            <!-- Sub Sub Categories -->
+                            <div class="sub-sub-category">
+                                <div class="hexagon" data-id="news">
+                                    <span class="hex-title" data-tippy-content="News & Events"><i
+                                            class="fa-regular fa-newspaper"></i></span>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="hexagon">
+                            <span class="hex-title" data-tippy-content="Our Team"><i
+                                    class="fa-solid fa-people-group"></i></span>
+                            <!-- Sub Sub Categories -->
+                            <div class="sub-sub-category">
+                                <div class="hexagon" data-id="team">
+                                    <span class="hex-title" data-tippy-content="Our Team"><i
+                                            class="fa-solid fa-people-group"></i></span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="hexagon main-hexagon animated">
+                    <span class="hex-title" data-tippy-content="Solutions"><i class="fa-solid fa-dna"></i></span>
+
+                    <!-- Sub Categories -->
+                    <div class="sub-categories">
+                        <div class="hexagon">
+                            <span class="hex-title" data-tippy-content="Cyber Security"><i
+                                    class="fa-solid fa-unlock"></i></span>
+
+                            <!-- Sub Sub Categories -->
+                            <div class="sub-sub-category">
+                                <div class="hexagon" data-id="emailSecurity">
+                                    <span class="hex-title" data-tippy-content="Email Security"><i
+                                            class="fa-regular fa-envelope"></i></span>
+                                </div>
+                                <div class="hexagon" data-id="risk">
+                                    <span class="hex-title" data-tippy-content="Risk & Compliance"><i
+                                            class="fa-solid fa-asterisk"></i></span>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="hexagon">
+                            <span class="hex-title" data-tippy-content="IT Management"><i
+                                    class="fa-solid fa-network-wired"></i></span>
+                            <!-- Sub Sub Categories -->
+                            <div class="sub-sub-category">
+                                <div class="hexagon" data-id="itManagement">
+                                    <span class="hex-title" data-tippy-content="IT Management & Analytics"><i
+                                            class="fa-solid fa-network-wired"></i></span>
+                                </div>
+                                <div class="hexagon" data-id="securityInfo">
+                                    <span class="hex-title" data-tippy-content="Security Information"><i
+                                            class="fa-solid fa-shield-halved"></i></span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="hexagon main-hexagon animated">
+                    <span class="hex-title" data-tippy-content="Services"><i class="fa-solid fa-gears"></i></span>
+                    <div class="sub-categories">
+                        <div class="hexagon"></div>
+                        <div class="hexagon"></div>
+                        <div class="hexagon"></div>
+                    </div>
+                </div>
+
+                <!-- Centered Hexagon -->
+                <div class="hexagon middle">
+                    <img src="{{ asset('assets/img/logo.png') }}" />
+                </div>
+
+                <div class="hexagon main-hexagon animated">
+                    <span class="hex-title" data-tippy-content="Man Power Services"><i
+                            class="fa-solid fa-user-gear"></i></span>
+                </div>
+                <div class="hexagon main-hexagon animated">
+                    <span class="hex-title" data-tippy-content="Management Services"><i
+                            class="fa-solid fa-users-gear"></i></span>
+                </div>
+                <div class="hexagon main-hexagon animated">
+                    <span class="hex-title"><i class="fa-brands fa-app-store"></i></span>
+                </div>
+            </div>
+
+            <!-- Right Side -->
+            <div class="right-side">
+                <div id="content">
+                    <div class="tab-content" id="careers">
+                        <h1 class="main-title">Careers</h1>
+                        <div class="desc">
+                            <p>
+                                this is sub sub ipsum dolor sit amet, consectetur adipisicing
+                                elit. Accusamus harum laborum unde! Quod nobis necessitatibus
+                                consequatur tempora impedit amet ratione vitae, aliquam
+                                doloremque libero eos quam et alias veritatis, pariatur.
+                            </p>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                                Accusamus harum laborum unde! Quod nobis necessitatibus
+                                consequatur tempor.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="tab-content" id="contact">
+                        <h1 class="main-title">Contact Us</h1>
+                    </div>
+                    <div class="tab-content" id="vision">
+                        <h1 class="main-title">Our Vision</h1>
+                        <div class="desc">
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                                Accusamus harum laborum unde! Quod nobis necessitatibus
+                                consequatur tempora impedit amet ratione vitae, aliquam
+                                doloremque libero eos quam et alias veritatis, pariatur.
+                            </p>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                                Accusamus harum laborum unde! Quod nobis necessitatibus
+                                consequatur tempor.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="tab-content" id="mission">
+                        <h1 class="main-title">Our Mission</h1>
+                        <div class="desc">
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                                Accusamus harum laborum unde! Quod nobis necessitatibus
+                                consequatur tempora impedit amet ratione vitae, aliquam
+                                doloremque libero eos quam et alias veritatis, pariatur.
+                            </p>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                                Accusamus harum laborum unde! Quod nobis necessitatibus
+                                consequatur tempor.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="tab-content" id="news">
+                        <h1 class="main-title">News &amp; Events</h1>
+                        <div class="desc">
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                                Accusamus harum laborum unde! Quod nobis necessitatibus
+                                consequatur tempora impedit amet ratione vitae, aliquam
+                                doloremque libero eos quam et alias veritatis, pariatur.
+                            </p>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                                Accusamus harum laborum unde! Quod nobis necessitatibus
+                                consequatur tempor.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="tab-content" id="team">
+                        <h1 class="main-title">Our Team</h1>
+                        <div class="desc">
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                                Accusamus harum laborum unde! Quod nobis necessitatibus
+                                consequatur tempora impedit amet ratione vitae, aliquam
+                                doloremque libero eos quam et alias veritatis, pariatur.
+                            </p>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                                Accusamus harum laborum unde! Quod nobis necessitatibus
+                                consequatur tempor.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="tab-content" id="emailSecurity">
+                        <h1 class="main-title">Email Security</h1>
+                        <div class="desc">
+                            <p>
+                                this is email psum dolor sit amet, consectetur adipisicing
+                                elit. Accusamus harum laborum unde! Quod nobis necessitatibus
+                                consequatur tempora impedit amet ratione vitae, aliquam
+                                doloremque libero eos quam et alias veritatis, pariatur.
+                            </p>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                                Accusamus harum laborum unde! Quod nobis necessitatibus
+                                consequatur tempor.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="tab-content" id="risk">
+                        <h1 class="main-title">Risk &amp; Compliance</h1>
+                        <div class="desc">
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                                Accusamus harum laborum unde! Quod nobis necessitatibus
+                                consequatur tempora impedit amet ratione vitae, aliquam
+                                doloremque libero eos quam et alias veritatis, pariatur.
+                            </p>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                                Accusamus harum laborum unde! Quod nobis necessitatibus
+                                consequatur tempor.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+
+<!-- Jquery -->
+<script src="{{ asset('assets/js/jquery-3.3.1.min.js') }}"></script>
+
+<!-- Tooltip -->
+<script src="https://unpkg.com/@popperjs/core@2"></script>
+<script src="https://unpkg.com/tippy.js@6"></script>
+
+<!-- Main Js -->
+<script src="{{ asset('assets/js/main.js') }}"></script>
+
+</html>
